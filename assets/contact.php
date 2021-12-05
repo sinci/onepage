d34488a4b2ad2eb37f671c12b4928631d1c45dec
sinci <office@sinci.at>
2021-12-05 23:37:31 +0100

    initial

---
+++ assets/contact.php
@@ -0,0 +1,51 @@
+<?php
+  
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $visitor_message = "";
+    $email_body = "<div>";
+
+    if(isset($_POST['visitor_name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    }
+      
+    $email_body .= "</div>";
+
+    $to = 'kian.caliskan@gmail.com';
+    $subject ='Contact Form';
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+      
+    if(mail($to,$subject,$email_body,$headers)) {
+        //  To redirect form on a particular page
+        header("Location:https://onepage.demoserver-a.eu/impressum");
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
+}
+?>
---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+// OFFCANVAS ON CLICK CLOSE
+UIkit.util.ready(function() {
+    UIkit.util.$$("#offcanvas-nav .uk-nav li a").forEach(function(el) {
+        if (
+            location.hostname == el.hostname &&
+            location.pathname == el.pathname &&
+            el.href != "#"
+        ) {
+            UIkit.scroll(el, { offset: 20 });
+            UIkit.util.on(el, "beforescroll", function() {
+                UIkit.offcanvas("#offcanvas-nav").hide();
+            });
+        }
+    });
+});
